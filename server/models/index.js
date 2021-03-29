714cff98cd3b8246aed31f252f237b23346aa799
Max Healy <maxhealy01@gmail.com>
2021-03-29 14:51:39 -0500

    Added all Model Types to typeDefs file.

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,9 @@
+const Activity = require("./Activity");
+const Class = require("./Class");
+const Goal = require("./Goal");
+const Message = require("./Message");
+const Testimonial = require("./Testimonial");
 const User = require("./User");
+const Workout = require("./Workout");
 
-module.exports = { User };
+module.exports = { Activity, Class, Goal, Message, Testimonial, User, Workout };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,53 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+	type Activity {
+		_id: ID
+		name: String!
+		type: String!
+	}
+	type Goal {
+		_id: ID
+		name: String!
+		metric: String!
+	}
+	type Class {
+		_id: ID
+		name: String!
+		location: String!
+		time: Date!
+		duration: String!
+		equipment: String
+		activity: Activity!
+		participants: [User]!
+		trainer: User
+	}
 	type User {
 		_id: ID
 		username: String
 		email: String
+		city: String
+		friends: [User]
+		goals: [Goal]
+		classes: [Class]
+		activities: [Activity]
+	}
+	type Testimonial {
+		postedBy: User!
+		text: String!
+	}
+	type Workout {
+		name: String!
+		source: String
+		duration: String!
+		equipment: String
+		activity: Activity
+		postedBy: User!
+	}
+	type Message {
+		message: String!
+		sentBy: User!
+		receivedBy: User!
 	}
 	type Auth {
 		token: ID

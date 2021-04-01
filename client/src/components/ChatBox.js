1ccd56518d85a8d3688be504d3054ca01661fa3f
Casey Deriso <caseyderiso@gmail.com>
2021-04-01 18:54:40 -0500

    finished chatReducer

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,33 +1,70 @@
-import React from 'react';
-import '../assets/scss/chatbox.scss';
-
+import React, { useState, useCallback } from "react";
+import "../assets/scss/chatbox.scss";
+import { useConversations } from "../utils/ConversationsProvider";
 
 const AppChatBox = () => {
+  const [text, setText] = useState("");
+  const setRef = useCallback((node) => {
+    if (node) {
+      node.scrollIntoView({ smooth: true });
+    }
+  }, []);
+  const { sendMessage, selectedConversation } = useConversations();
 
-	return (
-		<>
-			
-            <div className="chat">
-                <div className="chat-title">
-                    <h1>USER NAME HERE</h1>
-                    <h2>USER ID HERE</h2>
-                    <figure className="avatar">
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>
-                </div>
-                <div className="messages">
-                    <div className="messages-content"></div>
-                </div>
-                <div className="message-box">
-                    <textarea type="text" className="message-input" placeholder="Type message..."></textarea>
-                    <button type="submit" className="message-submit">Send</button>
-                </div>
+  function handleSubmit(e) {
+    e.preventDefault();
 
-            </div>
-            <div className="bg"></div>
-		</>
-	);
+    sendMessage(
+      selectedConversation.recipients.map((recip) => recip.id),
+      text
+    );
+    setText("");
+  }
+
+  return (
+    <>
+      <div className="chat">
+        <div className="chat-title">
+          <h1>USER NAME HERE</h1>
+          <h2>USER ID HERE</h2>
+          <figure className="avatar">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" />
+          </figure>
+        </div>
+        <div className="messages">
+          {selectedConversation.messages.map((message, index) => {
+            const lastMessage =
+              selectedConversation.messages.length - 1 === index;
+            return (
+              <div
+                ref={lastMessage ? setRef : null}
+                key={index}
+                className="messages-content"
+              >
+                {message.text}
+                <div classnName="messages-content small">
+                  {message.fromMe ? "You" : message.senderName}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="message-box">
+          <textarea
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="message-input"
+            placeholder="Type message..."
+          ></textarea>
+          <button type="submit" className="message-submit">
+            Send
+          </button>
+        </div>
+      </div>
+      <div className="bg"></div>
+    </>
+  );
 };
 
 export default AppChatBox;
-
-
--- client/src/utils/ConversationsProvider.js
+++ client/src/utils/ConversationsProvider.js
@@ -1,60 +1,106 @@
 import React, { useContext, useState, useEffect, useCallback } from "react";
 import { useSocket } from "./SocketProvider";
+import { useStoreContext } from "./GlobalState";
 
-const conversationsContext = React.createContext();
+const ConversationsContext = React.createContext();
 
 export function useConversations() {
-  return useContext(conversationsContext);
-};
+  return useContext(ConversationsContext);
+}
 
-export function onversationsProvider({ id, children }) {
+export function ConversationsProvider({ id, children }) {
   // get conversations from the database
-  let [conversations, setconversations]
+  let [conversations, setConversations] = useStoreContext();
   ////////
-  const [selectedConversationsIndex, setSelectedConversationsIndex] = useState(0);
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(
+    0
+  );
   const socket = useSocket();
 
-  function createconversations(recievedBy) {
-    setconversations(prevconversations => {
-      return [...prevconversations, { recievedBy, messages: [] }]
-    })
+  function createConversation(recievedBy) {
+    setConversations((prevconversations) => {
+      return [...prevconversations, { recievedBy, messages: [] }];
+    });
   }
 
-  const addMessageToConversation = useCallback(({ recievedBy, text, sentBy }) => {
-    setConversations(prevConversations => {
-      let madeChange = false
-      const newMessage = { sentBy, text }
-      const newConversations = prevConversations.map(conversation => {
-        if (arrayEquality(conversation.recievedBy, recievedBy)) {
-          madeChange = true
-          return {
-            ...conversation,
-            messages: [...conversation.messages, newMessage]
+  const addMessageToConversation = useCallback(
+    ({ recievedBy, text, sentBy }) => {
+      setConversations((prevConversations) => {
+        let madeChange = false;
+        const newMessage = { sentBy, text };
+        const newConversations = prevConversations.map((conversation) => {
+          if (arrayEquality(conversation.recievedBy, recievedBy)) {
+            madeChange = true;
+            return {
+              ...conversation,
+              messages: [...conversation.messages, newMessage],
+            };
           }
+
+          return conversation;
+        });
+
+        if (madeChange) {
+          return newConversations;
+        } else {
+          return [...prevConversations, { recievedBy, messages: [newMessage] }];
         }
-
-        return conversation
-      })
-
-      if (madeChange) {
-        return newConversations
-      } else {
-        return [
-          ...prevConversations,
-          { recievedBy, messages: [newMessage] }
-        ]
-      }
-    })
-  }, [setConversations])
+      });
+    },
+    [setConversations]
+  );
 
   useEffect(() => {
-    if (socket == null) return
+    if (socket == null) return;
 
-    socket.on("recieve-message", addMessageToConversation);
+    socket.on("receive-message", addMessageToConversation);
 
-    function sendMessage(sentBy)
-  }, [socket, addMessageToConversation])
+    return () => socket.off("receive-message");
+  }, [socket, addMessageToConversation]);
 
-  
+  function sendMessage(recipients, text) {
+    socket.emit("send-message", { recipients, text });
 
+    addMessageToConversation({ recipients, text, sender: id });
+  }
+
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      return { id: recipient };
+    });
+
+    const messages = conversation.messages.map((message) => {
+      const name =  message.sender;
+      const fromMe = id === message.sender;
+      return { ...message, senderName: name, fromMe };
+    });
+
+    const selected = index === selectedConversationIndex;
+    return { ...conversation, messages, recipients, selected };
+  });
+
+  const value = {
+    conversations: formattedConversations,
+    selectedConversation: formattedConversations[selectedConversationIndex],
+    sendMessage,
+    selectConversationIndex: setSelectedConversationIndex,
+    createConversation,
+  };
+
+  return (
+    <ConversationsContext.Provider value={value}>
+      {children}
+    </ConversationsContext.Provider>
+  );
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
+
+  a.sort();
+  b.sort();
+
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }

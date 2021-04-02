625b3ca8805b2ad3d422a120091af78837b72497
Casey Deriso <caseyderiso@gmail.com>
2021-04-02 16:14:37 -0500

    wrote actions and reducers for updating conversations in global state

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,1 +1,19 @@
-import gql from "graphql-tag";
+import { useReducer } from "react";
+
+import {
+  UPDATE_CONVERSATIONS
+} from "./actions"
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_CONVERSATIONS:
+      return {
+        ...state,
+        conversations: [...action.products],
+      }
+    default:
+      return {
+        state
+      }
+  }
+}

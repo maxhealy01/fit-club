9bd67a39d4558fd8fc1191837d9517f77d2d87d5
Casey Deriso <caseyderiso@gmail.com>
2021-04-01 19:59:16 -0500

    fixed fitnessClass model:

--- server/models/User.js
+++ server/models/User.js
@@ -6,65 +6,70 @@ const Activity = require("./Activity");
 const FitnessClass = require("./FitnessClass");
 
 const userSchema = new Schema(
-	{
-		username: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-			match: [/.+@.+\..+/, "Must use a valid email address"],
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		city: {
-			type: String,
-			trim: true,
-		},
-		friends: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: "User",
-			},
-		],
-		goals: [Goal.schema],
-		classes: [FitnessClass.schema],
-		activities: [Activity.schema],
-		isTrainer: {
-			type: Boolean,
-		},
-	},
-	// set this to use virtual below
-	{
-		toJSON: {
-			virtuals: true,
-			getters: true,
-		},
-	}
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      trim: true,
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    goals: [Goal.schema],
+    classes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "FitnessClass",
+      },
+    ],
+    activities: [Activity.schema],
+    isTrainer: {
+      type: Boolean,
+    },
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
 );
 
 // hash user password
 userSchema.pre("save", async function (next) {
-	if (this.isNew || this.isModified("password")) {
-		const saltRounds = 10;
-		this.password = await bcrypt.hash(this.password, saltRounds);
-	}
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-	next();
+  next();
 });
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
-	return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
 userSchema.virtual("friendCount").get(function () {
-	return this.friends.length;
+  return this.friends.length;
 });
 
 const User = model("User", userSchema);

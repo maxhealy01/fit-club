e1bfd5bd944f579798bc114f668561f963db0bf2
Mike Shelby <mikegshelby@gmail.com>
2021-04-03 11:25:49 -0500

    created GoalTracker component folder

---
+++ client/src/components/GoalTracker/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+
+const GoalTracker = () => {
+  return (
+    <div>
+        Goal Tracker
+
+    </div>
+  );
+};
+
+export default GoalTracker;

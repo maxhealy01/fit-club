aad822056ae2dbf43bfa5c6a422245df4cd07e4a
Max Healy <maxhealy01@gmail.com>
2021-04-06 19:38:20 -0500

    Created front-end queries.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,12 @@
 import gql from "graphql-tag";
 
 export const QUERY_ME = gql`
-  {
-    me {
-      _id
-    }
-  }
+	{
+		me {
+			_id
+		}
+	}
 `;
-
 
 ////////////////////////////////////////////
 // query goals with mikes data structure  //
@@ -31,10 +30,109 @@ export const QUERY_ME = gql`
 // `;
 
 export const QUERY_TRAINERS = gql`
-  {
-    trainers {
-      _id
-      username
-    }
-  }
+	{
+		trainers {
+			_id
+			username
+		}
+	}
+`;
+
+export const QUERY_USERS = gql`
+	{
+		users {
+			_id
+			username
+			email
+			city
+			friends {
+				username
+			}
+			meetups {
+				_id
+			}
+			activities {
+				_id
+				name
+			}
+			testimonials {
+				_id
+				text
+			}
+			isTrainer
+			messages {
+				_id
+			}
+		}
+	}
+`;
+
+export const QUERY_USER = gql`
+	query user($username: String!) {
+		user(username: $username) {
+			_id
+			username
+			email
+			city
+			friends {
+				username
+			}
+			meetups {
+				_id
+			}
+			activities {
+				_id
+				name
+			}
+			testimonials {
+				_id
+				text
+			}
+			isTrainer
+			messages {
+				_id
+			}
+		}
+	}
+`;
+
+export const QUERY_ACTIVITIES = gql`
+	{
+		activities {
+			_id
+			name
+			type
+		}
+	}
+`;
+
+export const QUERY_MEETUPS = gql`
+	{
+		meetups {
+			_id
+			name
+			location
+			duration
+		}
+	}
+`;
+
+export const QUERY_TESTIMONIALS = gql`
+	query testimonials($postedBy: ID) {
+		testimonials(postedBy: $postedBy) {
+			_id
+			text
+			postedBy
+		}
+	}
+`;
+
+export const QUERY_WORKOUTS = gql`
+	query workouts($activity: ID) {
+		workouts(activity: $activity) {
+			_id
+			name
+			duration
+		}
+	}
 `;

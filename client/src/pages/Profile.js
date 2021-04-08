b9efe1dd94c3bd2e6beb582e4274f36b98a2bcbf
Casey Deriso <caseyderiso@gmail.com>
2021-04-08 18:09:21 -0500

    uncommented chatbox component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -122,7 +122,7 @@ function Profile() {
      {!loading && 
       <SocketProvider id={_id}>
         <ConversationsProvider id={_id}>
-          {/* <ChatDashboard id={_id} /> */}
+          <ChatDashboard id={_id} />
         </ConversationsProvider>
       </SocketProvider> 
     } 
